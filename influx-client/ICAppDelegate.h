2e87eb993aa8b3c6c774ea3731d798ff10030fbd
Court Fowler <courtf@gmail.com>
2013-12-16 10:33:26 -0800

    Initial Commit

---
+++ influx-client/ICAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ICAppDelegate.h
+//  influx-client
+//
+//  Created by Courtf on 12/16/13.
+//  Copyright (c) 2013 ESRI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ICAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ influx-client/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  influx-client
+//
+//  Created by Courtf on 12/16/13.
+//  Copyright (c) 2013 ESRI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ICAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ICAppDelegate class]));
+    }
+}
